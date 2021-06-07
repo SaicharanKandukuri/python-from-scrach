05ecd592041d406ff1b2bfdee6e0b9f3236c9b4a
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-06-07 17:43:25 +0530

    updated seconds to dynamic

--- for_loop_contdown.py
+++ for_loop_contdown.py
@@ -1,7 +1,9 @@
 # for Loop -> statement that executes its code a limited amount of time
 import time
 
-for i in range(10,0,-1):
+seconds=int(input("Enter no.of seconds to countdown .. "))
+
+for i in range(seconds,0,-1):
     print(i)
     time.sleep(1)
-print("GG mate")
+print("\aYour "+str(seconds)+" timer is done!...")
