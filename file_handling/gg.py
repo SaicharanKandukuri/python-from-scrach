e4f1368c3aeba177960df5861bcd4476ccfab029
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-07-02 16:06:48 +0530

    privacy :)

--- file_handling/gg.py
+++ file_handling/gg.py
@@ -1,7 +1,7 @@
 
 line=str(input("menter something: "))
-f = open('D:\Apps & files\Codes\python-from-scrach\/file_handling\gg.txt','w')
+f = open('gg.txt','w')
 f.writelines(line)
 f.close()
-f = open('D:\Apps & files\Codes\python-from-scrach\/file_handling\gg.txt','r')
+f = open('gg.txt','r')
 print(f.readlines())
